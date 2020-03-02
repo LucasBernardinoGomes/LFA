4421cc663cb7c5c00e52d9393dbb914499a6d05d
LucasBernardinoGomes <52571655+LucasBernardinoGomes@users.noreply.github.com>
2020-03-02 09:35:32 -0300

    Update MaquinaDeVenda.java
    Valores alterados para inteiro

--- MaquinaDeVenda.java
+++ MaquinaDeVenda.java
@@ -5,17 +5,17 @@ public class MainApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double valorTotal = 0.45;
-		double valorInicial = 0;
+		int valorTotal = 45;
+		int valorInicial = 0;
 		Scanner valInput = new Scanner(System.in);
 		
 		do {
 			
 			System.out.println("Insira o valor da moeda: ");
 			String moeda = valInput.nextLine();
-			double valorMoeda = Double.parseDouble(moeda);
+			int valorMoeda = Integer.parseInt(moeda);
 			
-			if ( valorMoeda == 0.25  ||  valorMoeda == 0.10 ){
+			if ( valorMoeda == 25  ||  valorMoeda == 10 ){
 				valorInicial+=valorMoeda;
 					
 					if (valorInicial >= valorTotal) {
@@ -27,6 +27,6 @@ public class MainApplication {
 				System.out.println("Erro! Tipo de moeda não aceito.");
 			}
 				
-		}while(valorInicial <= valorTotal);		
+		}while(valorInicial < valorTotal);		
 	}
 }
