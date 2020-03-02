807da3ac5d919719a5e1848492eb67d31fe0eff6
LucasBernardinoGomes <52571655+LucasBernardinoGomes@users.noreply.github.com>
2020-03-02 09:11:04 -0300

    Update MaquinaDeVenda.java
    Melhoramentos

--- MaquinaDeVenda.java
+++ MaquinaDeVenda.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
 
-
 public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Boolean controle = true;
 		double valorTotal = 0.45;
 		double valorInicial = 0;
 		Scanner valInput = new Scanner(System.in);
@@ -16,25 +14,21 @@ public class MainApplication {
 			
 			System.out.println("Insira o valor da moeda: ");
 			String moeda  = valInput. nextLine();
-
 			double valorMoeda = Double.parseDouble(moeda);
 			
 			if ( valorMoeda == 0.25  ||  valorMoeda == 0.10 ){
 					valorInicial+=valorMoeda;
 					
 					if (valorInicial >= valorTotal) {
-							System.out.println("Aqui esta sua coca!");
-							controle=false;							
+						System.out.println("Aqui esta sua coca!");						
 					}
 			}
 			
-			
 			else {
 				System.out.println("Erro! Tipo de moeda não aceito.");
-			}
-					
+			}		
 				
-		}while(controle);
+		}while(true);
 			
 	}
 
